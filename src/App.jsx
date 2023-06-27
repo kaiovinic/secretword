aee9f48903ee356677b317840da80e1f88bb53db
kaiovinic <kaiovini19@hotmail.com>
2023-06-27 01:12:40 -0300

    Criação dos components Game e GameOver

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+// CSS
 import "./App.css";
+
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// Data
+import { wordList } from "./data/words";
+
+// Components
 import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordList);
+
+  // Starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
